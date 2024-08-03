ad5d15ec4db69dc0c2b4e6353fdde31bf0229cb7
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-08-03 16:49:26 +0200

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,39 @@
+import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react';
 
-const page = () => {
-  return <div>page</div>;
+const Home = () => {
+  const loggedIn = {
+    firstName: 'Arek',
+    lastName: 'Adamowicz',
+    email: 'arek.adamowicz81@gmail.com',
+  };
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions efficiently.'
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={4250.45}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 4123.5 }, { currentBalance: 2500 }]}
+      />
+    </section>
+  );
 };
 
-export default page;
+export default Home;
