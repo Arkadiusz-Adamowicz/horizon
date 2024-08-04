fd5677a0e48359c088a1394b31d6f354ce86be91
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-08-04 20:18:29 +0200

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,16 +12,22 @@ const Footer = ({ user, type = 'desktop' }: FooterProps) => {
   };
   return (
     <footer className='footer'>
-      <div className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}>
-        <p className='text-xl font-bold text-gray-700'>{user?.name[0]}</p>
-      </div>
-      <div className={type === 'mobile' ? 'email-mobile' : 'email'}>
-        <h1 className='text-14 truncate font-semibold text-gray-700'>
-          {user?.name}
-        </h1>
-        <p className='text-14 truncate font-normal text-gray-600'>
-          {user?.email}
-        </p>
+      <div className='flex justify-between w-full'>
+        <div className='flex gap-2'>
+          <div
+            className={type === 'mobile' ? 'footer_name-mobile' : 'footer_name'}
+          >
+            <p className='text-xl font-bold text-gray-700'>{user?.name[0]}</p>
+          </div>
+          <div className={type === 'mobile' ? 'email-mobile' : 'email'}>
+            <h1 className='text-14 truncate font-semibold text-gray-700'>
+              {user?.name}
+            </h1>
+            <p className='text-14 truncate font-normal text-gray-600'>
+              {user?.email}
+            </p>
+          </div>
+        </div>
       </div>
       <div className='footer_image' onClick={handleLogout}>
         <Image src='/icons/logout.svg' fill alt='logout' />
